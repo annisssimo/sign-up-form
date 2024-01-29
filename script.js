21eef8189e24b62bf3790b0e9314248dabc3b7d0
annisssimo <anyalar04@gmail.com>
2024-01-29 03:06:10 +0300

    feat: add password check

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const form = document.querySelector('form');
 const pwd = document.querySelector('#pwd');
 const pwdConfirm = document.querySelector('#pwd-confirm');
 const message = document.querySelector('.message');
+const inputConfirmPwd = document.querySelector('#pwd-confirm');
 
-form.addEventListener('submit', function (event) {
-    if (pwd.value !== pwdConfirm.value) {
-        message.textContent = 'Passwords do not match. Please try again.';
+form.addEventListener('submit', function(event) {
+    if(pwdConfirm.value !== pwd.value) {
         event.preventDefault();
+        inputConfirmPwd.style.color = 'var(--red)';
+        message.textContent = 'Passwords do not match :(';
+    }
+});
+
+pwdConfirm.addEventListener('input', (event) => {
+    if(pwdConfirm.value === pwd.value) {
+        message.textContent = '';
+        inputConfirmPwd.style.color = 'var(--black)';
     }
 });
