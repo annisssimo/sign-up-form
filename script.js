4198198249784f956f06cb8d1226ea1b3a54c270
annisssimo <anyalar04@gmail.com>
2024-01-29 18:09:22 +0300

    feat: add red error messages when the field is empty

--- script.js
+++ script.js
@@ -1,20 +1,102 @@
 const form = document.querySelector('form');
+
+//Inputs
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
 const pwd = document.querySelector('#pwd');
 const pwdConfirm = document.querySelector('#pwd-confirm');
-const message = document.querySelector('.message');
-const inputConfirmPwd = document.querySelector('#pwd-confirm');
+
+//Error messages for each input
+const firstNameErrMessage = document.querySelector('#first-name + .error-message');
+const lastNameErrMessage = document.querySelector('#last-name + .error-message');
+const emailErrMessage = document.querySelector('#email + .error-message');
+const phoneErrMessage = document.querySelector('#phone + .error-message');
+const pwdErrMessage = document.querySelector('#pwd + .error-message');
+const pwdConfirmErrMessage = document.querySelector('#pwd-confirm + .error-message');
+
 
 form.addEventListener('submit', function(event) {
+    if(firstName.value === '') {
+        event.preventDefault();
+        firstName.classList.add('error');
+        firstNameErrMessage.textContent = 'First name is required';
+    }
+
+    if(lastName.value === '') {
+        event.preventDefault();
+        lastNameErrMessage.textContent = 'Last name is required';
+    }
+
+    if(email.value === '') {
+        event.preventDefault();
+        emailErrMessage.textContent = 'Email name is required';
+    }
+
+    if(phone.value === '') {
+        event.preventDefault();
+        phoneErrMessage.textContent = 'Phone is required';
+    }
+
+    if(pwd.value === '') {
+        event.preventDefault();
+        pwdErrMessage.textContent = 'Password is required';
+    }
+
+    if(pwdConfirm.value === '') {
+        event.preventDefault();
+        pwdConfirmErrMessage.textContent = 'Please, confirm your password';
+    }
+
     if(pwdConfirm.value !== pwd.value) {
         event.preventDefault();
-        inputConfirmPwd.style.color = 'var(--red)';
-        message.textContent = 'Passwords do not match :(';
+        pwdConfirm.style.color = 'var(--red)';
+        pwdConfirmErrMessage.textContent = 'Passwords do not match :(';
+    }
+});
+
+firstName.addEventListener('input', (event) => {
+    if(firstName.value != '') {
+        firstNameErrMessage.textContent = '';
+    }
+});
+
+lastName.addEventListener('input', (event) => {
+    if(lastName.value != '') {
+        lastNameErrMessage.textContent = '';
+    }
+});
+
+email.addEventListener('input', (event) => {
+    if(email.value != '') {
+        emailErrMessage.textContent = '';
+    }
+});
+
+phone.addEventListener('input', (event) => {
+    if(phone.value != '') {
+        phoneErrMessage.textContent = '';
+    }
+});
+
+pwd.addEventListener('input', (event) => {
+    if(pwd.value != '') {
+        pwdErrMessage.textContent = '';
     }
 });
 
 pwdConfirm.addEventListener('input', (event) => {
-    if(pwdConfirm.value === pwd.value) {
-        message.textContent = '';
-        inputConfirmPwd.style.color = 'var(--black)';
+    if(pwdConfirm.value != '') {
+        pwdConfirmErrMessage.textContent = '';
     }
 });
+
+
+pwdConfirm.addEventListener('input', (event) => {
+    if(pwdConfirm.value === pwd.value) {
+        pwdConfirmErrMessage.textContent = '';
+        pwdConfirm.style.color = 'var(--black)';
+    }
+});
+
